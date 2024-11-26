8e78ba041f6b8681a8beac8a034ddf6274f60632
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2024-11-26 13:41:52 +0100

    initial commit

---
+++ sightseeing_app/README.md
@@ -0,0 +1,3 @@
+# sightseeing_app
+
+A new Flutter project.
---
+++ sightseeing_app/android/app/src/main/kotlin/com/example/sightseeing_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sightseeing_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
