329051d0a5fae67dfef5de72a24ac889a78ac055
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2024-11-28 18:28:09 +0100

    Feature/add start page (#2)
    * feat: add start page
    
    * chore: move compass to main_page.dart

---
+++ sightseeing_app/lib/pages/start_page.dart
@@ -0,0 +1,141 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class StartPage extends StatelessWidget {
+  const StartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    precacheImage(const AssetImage('assets/map.jpg'), context);
+    return MaterialApp(
+      title: 'Sightseeing App',
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/map.jpg'),
+                  fit: BoxFit.cover,
+                  opacity: 1.0,
+                  colorFilter: ColorFilter.mode(
+                      Colors.black.withOpacity(0.5), BlendMode.darken))),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Sightseein App',
+                    style: TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        shadows: [
+                          Shadow(
+                            blurRadius: 10.0,
+                            color: Colors.blue,
+                            offset: Offset(5, 5),
+                          ),
+                        ]),
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Expanded(
+                            child: ElevatedButton(
+                                onPressed: () => print('Start'),
+                                style: TextButton.styleFrom(
+                                  backgroundColor: Colors.blue,
+                                  elevation: 8,
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 16, 0, 16),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8)),
+                                  disabledBackgroundColor: Colors.grey,
+                                ),
+                                child: Text(
+                                  'Start',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 24),
+                                )))
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Expanded(
+                            child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 24),
+                          child: ElevatedButton(
+                              onPressed: () => print('Resume'),
+                              style: TextButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                side: BorderSide(
+                                    color: Colors.blue,
+                                    width: 2,
+                                    strokeAlign: BorderSide.strokeAlignOutside),
+                                elevation: 8,
+                                padding: EdgeInsetsDirectional.fromSTEB(
+                                    0, 16, 0, 16),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8)),
+                                disabledBackgroundColor: Colors.grey,
+                              ),
+                              child: Text(
+                                'Resume',
+                                style:
+                                    TextStyle(color: Colors.blue, fontSize: 24),
+                              )),
+                        ))
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Expanded(
+                            child: ElevatedButton(
+                                onPressed: () => print('History'),
+                                style: TextButton.styleFrom(
+                                  backgroundColor: Colors.white,
+                                  side: BorderSide(
+                                      color: Colors.blue,
+                                      width: 2,
+                                      strokeAlign:
+                                          BorderSide.strokeAlignOutside),
+                                  elevation: 8,
+                                  padding: EdgeInsetsDirectional.fromSTEB(
+                                      0, 16, 0, 16),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  disabledBackgroundColor: Colors.grey,
+                                ),
+                                child: Text(
+                                  'History',
+                                  style: TextStyle(
+                                      color: Colors.blue, fontSize: 24),
+                                )))
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
