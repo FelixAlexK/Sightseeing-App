8a29461c10628859c35294787498f2d56fa95a85
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2024-11-29 20:44:32 +0100

    feat: update main app structure and enhance start page layout

--- sightseeing_app/lib/main.dart
+++ sightseeing_app/lib/main.dart
@@ -2,4 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'pages/start_page.dart';
 
-void main() => runApp(StartPage());
+void main() => runApp(MaterialApp(
+      title: 'Sighseeing App',
+      home: StartPage(),
+    ));
